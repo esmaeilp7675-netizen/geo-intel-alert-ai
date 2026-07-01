6c5d7089e7e67e885c670d1ca34ee54665fa409d
esmaeilp7675-netizen <esmaeil.p7675@gmail.com>
2026-07-01 21:06:29 +0330

    Update app.py

--- app.py
+++ app.py
@@ -39,3 +39,13 @@ while True:
     except Exception as e:
         print("Error:", e)
         time.sleep(10)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Geo Intel AI is running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
